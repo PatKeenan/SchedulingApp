c4ce5e049e0bf88c2f25b82f159d93171b431f12
PatKeenan <patkeenan.dev@gmail.com>
2021-04-22 09:59:06 -0400

    [ISSUE]: trying to figure out why the buttons lose their color on deploy but not in dev

--- webapp/scheduling-app/src/components/UI/Button/index.jsx
+++ webapp/scheduling-app/src/components/UI/Button/index.jsx
@@ -1,10 +1,10 @@
 const Index = ({ title, variant, func }) => {
-  let color = variant;
+  let color = `bg-${variant}-500`;
   return (
     <>
       {variant ? (
         <button
-          className={`bg-${color}-500 text-white shadow-md px-4 py-2 rounded`}
+          className={`${color} text-white shadow-md px-4 py-2 rounded`}
           onClick={func}
         >
           {title}
